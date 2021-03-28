9a689b33639ea7b0723b8c92fd216dc82d3d0ce1
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-28 12:13:54 +0900

    useClick 사용하기
    //useClick 사용해서 글자누르면 console에 명령어 호출되게 만들기.

---
+++ #USEEFFECT/#2.2 useClick.js
@@ -0,0 +1,35 @@
+//useClick 사용해서 글자누르면 console에 명령어 호출되게 만들기.
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+    const useClick = (onClick) => {
+      if (typeof onClick !== "function") {
+        return;
+      }
+      const element = useRef();
+      useEffect(() => {
+        if (element.current) {
+          element.current.addEventListener("click", onClick);
+        }
+        return () => {
+          if (element.current) {
+            element.current.removeEventListener("click", onClick);
+          }
+        };
+      }, []);
+        return element;
+    };
+
+    const App = () => {
+      const saysooggwa = () => console.log("수꽈수꽈안녕하수꽈");
+      const title = useClick(saysooggwa);
+      return (
+        <div className="App">
+          <h1 ref={title}>안녕하수꽈</h1>
+        </div>
+      );
+    };
+
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(<App />, rootElement);
